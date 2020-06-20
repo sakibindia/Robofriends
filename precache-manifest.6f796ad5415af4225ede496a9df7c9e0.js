27704dc6a44fed701d20a09b99ce3cbc1a402303
GISSoftware <mohdsakibimran@gmail.com>
2020-06-20 19:23:40 +0530

    Updates

---
+++ precache-manifest.6f796ad5415af4225ede496a9df7c9e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5bb9cf18c5faa7b2fb15e445b253d4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9434c276396b3f96ab37",
+    "url": "/static/css/main.9a6dc019.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9434c276396b3f96ab37",
+    "url": "/static/js/main.cc324cb6.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
