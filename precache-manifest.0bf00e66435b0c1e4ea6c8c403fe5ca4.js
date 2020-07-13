55e1c97d57f9f372059db2cb30eb0a1d165efb03
GISSoftware <mohdsakibimran@gmail.com>
2020-07-13 08:56:44 +0530

    Updates

---
+++ precache-manifest.0bf00e66435b0c1e4ea6c8c403fe5ca4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4afe601a1c7dc5273ef7eed49d2323",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "7acd90d33eae3a38d832",
+    "url": "/Robofriends/static/css/2.c6861289.chunk.css"
+  },
+  {
+    "revision": "67de917eeecb510731d8",
+    "url": "/Robofriends/static/css/main.bff96de7.chunk.css"
+  },
+  {
+    "revision": "7acd90d33eae3a38d832",
+    "url": "/Robofriends/static/js/2.ddd16248.chunk.js"
+  },
+  {
+    "revision": "4a8cddea50fefad5b6f84ace2f7f0a62",
+    "url": "/Robofriends/static/js/2.ddd16248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67de917eeecb510731d8",
+    "url": "/Robofriends/static/js/main.2739a286.chunk.js"
+  },
+  {
+    "revision": "9eff52c4d1627d87e058",
+    "url": "/Robofriends/static/js/runtime-main.b221b423.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21053f77a95807f6eae4e22c698019e0.js"
+  "/Robofriends/precache-manifest.0bf00e66435b0c1e4ea6c8c403fe5ca4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Robofriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
